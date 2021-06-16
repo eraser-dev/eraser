a17a417ca68ff6fc565a981afce371e9d65db948
ashnamehrotra <ashnamehrotra@gmail.com>
2021-06-16 03:59:43 -0700

    changes with cri api

--- eraser.go
+++ eraser.go
@@ -2,17 +2,36 @@ package main
 
 import (
 	"context"
-	"flag"
+	//	"flag"
 	"fmt"
+	"time"
 
-	"github.com/containerd/containerd"
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
+	//	"github.com/containerd/containerd"
+	//	"github.com/docker/docker/api/types"
+	//	"github.com/docker/docker/client"
+
+	cli "github.com/urfave/cli/v2"
+	"google.golang.org/grpc"
+	pb "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
+
+	"net"
+	"net/url"
 )
+
+const (
+	// unixProtocol is the network protocol of unix socket.
+	unixProtocol = "unix"
+)
+
+var (
+	// Timeout  of connecting to server (default: 10s)
+	Timeout time.Duration
+)
+
+/*
 
 func getContainerdImages() error {
 
-	// namespace k8s.io in aks, default otherwise
 	client, err := containerd.New("/run/containerd/containerd.sock", containerd.WithDefaultNamespace("k8s.io"))
 	if err != nil {
 		return err
@@ -22,6 +41,9 @@ func getContainerdImages() error {
 	ctx := context.Background()
 
 	list, err := client.ListImages(ctx)
+	if err != nil {
+		return err
+	}
 
 	for _, elm := range list {
 		fmt.Println(elm.Name())
@@ -43,24 +65,186 @@ func getDockerImages() error {
 	}
 
 	for _, image := range images {
-		fmt.Println(image.ID)
+		fmt.Println(image.RepoTags)
 	}
 
 	return nil
 
 }
 
+*/
+
+func GetAddressAndDialer(endpoint string) (string, func(ctx context.Context, addr string) (net.Conn, error), error) {
+	protocol, addr, err := parseEndpointWithFallbackProtocol(endpoint, unixProtocol)
+	if err != nil {
+		return "", nil, err
+	}
+	if protocol != unixProtocol {
+		return "", nil, fmt.Errorf("only support unix socket endpoint")
+	}
+
+	return addr, dial, nil
+}
+
+func dial(ctx context.Context, addr string) (net.Conn, error) {
+	return (&net.Dialer{}).DialContext(ctx, unixProtocol, addr)
+}
+
+func parseEndpointWithFallbackProtocol(endpoint string, fallbackProtocol string) (protocol string, addr string, err error) {
+	if protocol, addr, err = parseEndpoint(endpoint); err != nil && protocol == "" {
+		fallbackEndpoint := fallbackProtocol + "://" + endpoint
+		protocol, addr, err = parseEndpoint(fallbackEndpoint)
+		if err == nil {
+			//klog.InfoS("Using this endpoint is deprecated, please consider using full URL format", "endpoint", endpoint, "URL", fallbackEndpoint)
+		}
+	}
+	return
+}
+
+func parseEndpoint(endpoint string) (string, string, error) {
+	u, err := url.Parse(endpoint)
+	if err != nil {
+		return "", "", err
+	}
+
+	switch u.Scheme {
+	case "tcp":
+		return "tcp", u.Host, nil
+
+	case "unix":
+		return "unix", u.Path, nil
+
+	case "":
+		return "", "", fmt.Errorf("using %q as endpoint is deprecated, please consider using full url format", endpoint)
+
+	default:
+		return u.Scheme, "", fmt.Errorf("protocol %q not supported", u.Scheme)
+	}
+}
+
+func getImageClient(context *cli.Context) (pb.ImageServiceClient, *grpc.ClientConn, error) {
+	addr, dialer, err := GetAddressAndDialer("unix:///run/containerd/containerd.sock")
+
+	if err != nil {
+		fmt.Print("get address and dialer err")
+	}
+
+	conn, err := grpc.Dial(addr, grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(Timeout), grpc.WithContextDialer(dialer))
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	imageClient := pb.NewImageServiceClient(conn)
+
+	return imageClient, conn, nil
+}
+
+func ListImages(client pb.ImageServiceClient, image string) (resp *pb.ListImagesResponse, err error) {
+	request := &pb.ListImagesRequest{Filter: &pb.ImageFilter{Image: &pb.ImageSpec{Image: image}}}
+	//logrus.Debugf("ListImagesRequest: %v", request)
+
+	resp, err = client.ListImages(context.Background(), request)
+	//logrus.Debugf("ListImagesResponse: %v", resp)
+
+	return
+}
+
+func RemoveImage(client pb.ImageServiceClient, image string) (resp *pb.RemoveImageResponse, err error) {
+	if image == "" {
+		return nil, fmt.Errorf("ImageID cannot be empty")
+	}
+	request := &pb.RemoveImageRequest{Image: &pb.ImageSpec{Image: image}}
+	//logrus.Debugf("RemoveImageRequest: %v", request)
+
+	resp, err = client.RemoveImage(context.Background(), request)
+	//logrus.Debugf("RemoveImageResponse: %v", resp)
+	return
+}
+
+func contains(slice []string, str string) bool {
+	for _, s := range slice {
+		if s == str {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 
-	runtimePtr := flag.String("runtime", "containerd", "container runtime")
-	flag.Parse()
+	/*
+		runtimePtr := flag.String("runtime", "containerd", "container runtime")
 
-	if *runtimePtr == "docker" {
-		getDockerImages()
+		flag.Parse()
+
+		if *runtimePtr == "docker" {
+			getDockerImages()
+		}
+
+		if *runtimePtr == "containerd" {
+			getContainerdImages()
+		}
+	*/
+
+	// using CRICTL
+
+	ctx := cli.NewContext(nil, nil, nil)
+
+	imageClient, conn, err := getImageClient(ctx)
+
+	if err != nil {
+		fmt.Printf("image client err")
 	}
 
-	if *runtimePtr == "containerd" {
-		getContainerdImages()
+	r, err := ListImages(imageClient, "")
+	if err != nil {
+		fmt.Printf("list err")
 	}
+
+	var allImages []string
+
+	for _, img := range r.Images {
+		allImages = append(allImages, img.Id)
+	}
+
+	response, err := pb.NewRuntimeServiceClient(conn).ListContainers(context.Background(), new(pb.ListContainersRequest))
+
+	if err != nil {
+		fmt.Printf("list containers err")
+	}
+
+	var runningImages []string
+
+	for _, container := range response.Containers {
+		runningImages = append(runningImages, container.ImageRef)
+	}
+
+	var nonRunningImages []string
+
+	for _, img := range allImages {
+		if !contains(runningImages, img) {
+			nonRunningImages = append(nonRunningImages, img)
+		}
+	}
+
+	// quick remove test
+
+	fmt.Println("All images: ")
+	fmt.Println(len(allImages))
+
+	fmt.Println("Running images: ")
+	fmt.Println(len(runningImages))
+
+	fmt.Println("non running images: ")
+	fmt.Println(len(nonRunningImages))
+
+	fmt.Println("Removing first non-running image ...")
+	RemoveImage(imageClient, nonRunningImages[0])
+
+	r, err = ListImages(imageClient, "")
+
+	fmt.Println("New All images total: ")
+	fmt.Println(len(r.Images))
 
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/Azure/eraser
 go 1.13
 
 require (
-	github.com/containerd/containerd v1.5.2
-	github.com/docker/docker v20.10.7+incompatible
-	github.com/docker/go-connections v0.4.0 // indirect
-	github.com/morikuni/aec v1.0.0 // indirect
-	k8s.io/client-go v0.22.0-alpha.3
+	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
+	github.com/google/go-cmp v0.5.4 // indirect
+	github.com/urfave/cli/v2 v2.3.0
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/sys v0.0.0-20210324051608-47abb6519492 // indirect
+	google.golang.org/grpc v1.33.2
+	k8s.io/cri-api v0.21.1
 )
