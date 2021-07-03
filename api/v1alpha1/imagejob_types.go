9d517e058348d6c1a2d92863be24c6a600547bed
ashnamehrotra <ashnamehrotra@gmail.com>
2021-07-03 07:07:53 -0700

    created ImageStatus CRD, made more changes for adding alpha1 to v1 imports

--- api/v1alpha1/imagejob_types.go
+++ api/v1alpha1/imagejob_types.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package v1
+package v1alpha1
 
 import (
 	batchv1alpha1 "k8s.io/api/batch/v1alpha1"
