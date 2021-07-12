fc58d9fb7d7755d15953b67721ef060a71f86137
ashnamehrotra <ashnamehrotra@gmail.com>
2021-07-12 01:24:54 -0700

    remove jobspec type from imagejob

--- api/v1alpha1/imagejob_types.go
+++ api/v1alpha1/imagejob_types.go
@@ -24,9 +24,6 @@ import (
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
-type JobSpec struct {
-}
-
 // ImageJobSpec defines the desired state of ImageJob
 type ImageJobSpec struct {
 	// Specifies the job that will be created when executing an ImageJob.
