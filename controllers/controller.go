028a89c4cd1cc997f524184ec7381fc8b091d02f
ashnamehrotra <ashnamehrotra@gmail.com>
2021-07-16 10:59:36 -0700

    change to controller runtime log

--- controllers/controller.go
+++ controllers/controller.go
@@ -4,11 +4,14 @@ import (
 	"github.com/Azure/eraser/controllers/imagejob"
 	"github.com/Azure/eraser/controllers/imagelist"
 	"k8s.io/apimachinery/pkg/api/meta"
-	"k8s.io/klog"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
-var controllerAddFuncs []func(manager.Manager) error
+var (
+	controllerLog      = ctrl.Log.WithName("controllerRuntimeLogger")
+	controllerAddFuncs []func(manager.Manager) error
+)
 
 func init() {
 	controllerAddFuncs = append(controllerAddFuncs, imagelist.Add)
@@ -19,7 +22,7 @@ func SetupWithManager(m manager.Manager) error {
 	for _, f := range controllerAddFuncs {
 		if err := f(m); err != nil {
 			if kindMatchErr, ok := err.(*meta.NoKindMatchError); ok {
-				klog.Infof("CRD %v is not installed, its controller will perform noops!", kindMatchErr.GroupKind)
+				controllerLog.Info("CRD %v is not installed, its controller will perform noops!", kindMatchErr.GroupKind)
 				continue
 			}
 			return err
