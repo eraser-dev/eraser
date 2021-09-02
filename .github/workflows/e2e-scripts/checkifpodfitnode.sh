8de1b7db2139abb300e267a72be61d1f0260d280
AldairCoronel <aldaircr@ciencias.unam.mx>
2021-09-02 17:22:32 -0500

    feat: Add e2e scripts

---
+++ .github/workflows/e2e-scripts/checkifpodfitnode.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+podNames=$(kubectl get pods -n eraser-system | grep eraser-e2e-cluster | awk '{print $1}')
+
+for podName in ${podNames}; do
+    $(kubectl get events -n eraser-system --field-selector involvedObject.name="$podName" >> outputevent.txt)
+    
+    if grep -q "fits the node" outputevent.txt; then
+        echo "$podName fit!"
+        rm outputevent.txt
+    else
+        echo "$podName did not fit"
+        rm outputevent.txt
+        exit 1
+    fi
+done
---
+++ .github/workflows/e2e-scripts/checkifpodsarecomplete.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+$(kubectl get pod --field-selector=status.phase=Succeeded -n eraser-system >> outputcompleted.txt)
+
+numNodes=$(($(kubectl get nodes | wc -l) - 1))
+numPodsCompleted=$(grep -c Completed outputcompleted.txt)
+numNamePodsCompleted=$(grep -c eraser-e2e-cluster outputcompleted.txt)
+
+if [ $numPodsCompleted -ge $numNodes ] && [ $numNamePodsCompleted -ge $numNodes ]; then
+    echo "All pods completed"
+    rm outputcompleted.txt
+    exit 0
+else
+    echo "Not all pods completed"
+    rm outputcompleted.txt
+    exit 1
+fi
