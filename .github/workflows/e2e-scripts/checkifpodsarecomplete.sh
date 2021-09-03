32db744323561556eeb1fe64bf9cfcc304b819b3
AldairCoronel <aldaircr@ciencias.unam.mx>
2021-09-03 10:48:25 -0500

    feat: Add quotes to variables declaration

--- .github/workflows/e2e-scripts/checkifpodsarecomplete.sh
+++ .github/workflows/e2e-scripts/checkifpodsarecomplete.sh
@@ -2,9 +2,9 @@
 
 $(kubectl get pod --field-selector=status.phase=Succeeded -n eraser-system >> outputcompleted.txt)
 
-numNodes=$(($(kubectl get nodes | wc -l) - 1))
-numPodsCompleted=$(grep -c Completed outputcompleted.txt)
-numNamePodsCompleted=$(grep -c eraser-${1} outputcompleted.txt)
+numNodes="$(($(kubectl get nodes | wc -l) - 1))"
+numPodsCompleted="$(grep -c Completed outputcompleted.txt)"
+numNamePodsCompleted="$(grep -c eraser-${1} outputcompleted.txt)"
 
 if [ $numPodsCompleted -ge $numNodes ] && [ $numNamePodsCompleted -ge $numNodes ]; then
     echo "All pods completed"
