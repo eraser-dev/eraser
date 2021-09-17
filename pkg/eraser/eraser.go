a62ed13d6ffee8ebcb855f502b67922fc6dcba6a
AldairCoronel <aldaircr@ciencias.unam.mx>
2021-09-17 11:20:32 -0500

    WIP refactor

--- pkg/eraser/eraser.go
+++ pkg/eraser/eraser.go
@@ -3,147 +3,18 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"flag"
 	"log"
 	"os"
 
-	"fmt"
-	"time"
-
-	"google.golang.org/grpc"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	pb "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
 
-	"net"
-	"net/url"
-
 	eraserv1alpha1 "github.com/Azure/eraser/api/v1alpha1"
+	"github.com/Azure/eraser/pkg/util"
 )
-
-const (
-	// unixProtocol is the network protocol of unix socket.
-	unixProtocol = "unix"
-	apiPath      = "apis/eraser.sh/v1alpha1"
-)
-
-var (
-	// Timeout  of connecting to server (default: 10s)
-	timeout                  = 10 * time.Second
-	ErrProtocolNotSupported  = errors.New("protocol not supported")
-	ErrEndpointDeprecated    = errors.New("endpoint is deprecated, please consider using full url format")
-	ErrOnlySupportUnixSocket = errors.New("only support unix socket endpoint")
-)
-
-type client struct {
-	images  pb.ImageServiceClient
-	runtime pb.RuntimeServiceClient
-}
-
-type Client interface {
-	listImages(context.Context) ([]*pb.Image, error)
-	listContainers(context.Context) ([]*pb.Container, error)
-	deleteImage(context.Context, string) error
-}
-
-func (c *client) listContainers(context.Context) (list []*pb.Container, err error) {
-	resp, err := c.runtime.ListContainers(context.Background(), new(pb.ListContainersRequest))
-	if err != nil {
-		return nil, err
-	}
-	return resp.Containers, nil
-}
-
-func (c *client) listImages(ctx context.Context) (list []*pb.Image, err error) {
-	request := &pb.ListImagesRequest{Filter: nil}
-
-	resp, err := c.images.ListImages(ctx, request)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp.Images, nil
-}
-
-func (c *client) deleteImage(ctx context.Context, image string) (err error) {
-	if image == "" {
-		return err
-	}
-
-	request := &pb.RemoveImageRequest{Image: &pb.ImageSpec{Image: image}}
-
-	_, err = c.images.RemoveImage(ctx, request)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func GetAddressAndDialer(endpoint string) (string, func(ctx context.Context, addr string) (net.Conn, error), error) {
-	protocol, addr, err := parseEndpointWithFallbackProtocol(endpoint, unixProtocol)
-	if err != nil {
-		return "", nil, err
-	}
-	if protocol != unixProtocol {
-		return "", nil, ErrOnlySupportUnixSocket
-	}
-
-	return addr, dial, nil
-}
-
-func dial(ctx context.Context, addr string) (net.Conn, error) {
-	return (&net.Dialer{}).DialContext(ctx, unixProtocol, addr)
-}
-
-func parseEndpointWithFallbackProtocol(endpoint string, fallbackProtocol string) (protocol string, addr string, err error) {
-	if protocol, addr, err = parseEndpoint(endpoint); err != nil && protocol == "" {
-		fallbackEndpoint := fallbackProtocol + "://" + endpoint
-		protocol, addr, err = parseEndpoint(fallbackEndpoint)
-		if err != nil {
-			return "", "", err
-		}
-	}
-	return protocol, addr, err
-}
-
-func parseEndpoint(endpoint string) (string, string, error) {
-	u, err := url.Parse(endpoint)
-	if err != nil {
-		return "", "", fmt.Errorf("error while parsing: %w", err)
-	}
-
-	switch u.Scheme {
-	case "tcp":
-		return "tcp", u.Host, nil
-	case "unix":
-		return "unix", u.Path, nil
-
-	case "":
-		return "", "", fmt.Errorf("using %q as %w", endpoint, ErrEndpointDeprecated)
-
-	default:
-		return u.Scheme, "", fmt.Errorf("%q: %w", u.Scheme, ErrProtocolNotSupported)
-	}
-}
-
-func getImageClient(ctx context.Context, socketPath string) (pb.ImageServiceClient, *grpc.ClientConn, error) {
-	addr, dialer, err := GetAddressAndDialer(socketPath)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	conn, err := grpc.DialContext(ctx, addr, grpc.WithBlock(), grpc.WithInsecure(), grpc.WithContextDialer(dialer))
-	if err != nil {
-		return nil, nil, err
-	}
-
-	imageClient := pb.NewImageServiceClient(conn)
-
-	return imageClient, conn, nil
-}
 
 func updateStatus(ctx context.Context, clientset *kubernetes.Clientset, results []eraserv1alpha1.NodeCleanUpDetail) error {
 	imageStatus := eraserv1alpha1.ImageStatus{
@@ -304,14 +175,14 @@ func main() {
 		log.Fatal("incorrect runtime")
 	}
 
-	imageclient, conn, err := getImageClient(context.Background(), socketPath)
+	imageclient, conn, err := util.GetImageClient(context.Background(), socketPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	runTimeClient := pb.NewRuntimeServiceClient(conn)
 
-	client := &client{imageclient, runTimeClient}
+	client := &util.client{imageclient, runTimeClient}
 
 	// get list of images to remove from ImageList
 	var targetImages []string
