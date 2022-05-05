9df07514a0ad42308b70f3567f14f636867a77eb
ashnamehrotra <ashnamehrotra@gmail.com>
2022-05-05 12:13:14 -0400

    collector file and deployment yaml for test

---
+++ pkg/collector/collector.go
@@ -0,0 +1,183 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"net"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/Azure/eraser/pkg/logger"
+	"google.golang.org/grpc"
+	pb "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+const (
+	// unixProtocol is the network protocol of unix socket.
+	unixProtocol = "unix"
+)
+
+var (
+	// Timeout  of connecting to server (default: 5m).
+	timeout                  = 5 * time.Minute
+	errProtocolNotSupported  = errors.New("protocol not supported")
+	errEndpointDeprecated    = errors.New("endpoint is deprecated, please consider using full url format")
+	errOnlySupportUnixSocket = errors.New("only support unix socket endpoint")
+	log                      = logf.Log.WithName("eraser")
+)
+
+type client struct {
+	images  pb.ImageServiceClient
+	runtime pb.RuntimeServiceClient
+}
+
+type Client interface {
+	listImages(context.Context) ([]*pb.Image, error)
+}
+
+func (c *client) listImages(ctx context.Context) (list []*pb.Image, err error) {
+	request := &pb.ListImagesRequest{Filter: nil}
+
+	resp, err := c.images.ListImages(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Images, nil
+}
+
+func getImageClient(ctx context.Context, socketPath string) (pb.ImageServiceClient, *grpc.ClientConn, error) {
+	addr, dialer, err := GetAddressAndDialer(socketPath)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	conn, err := grpc.DialContext(ctx, addr, grpc.WithBlock(), grpc.WithInsecure(), grpc.WithContextDialer(dialer))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	imageClient := pb.NewImageServiceClient(conn)
+
+	return imageClient, conn, nil
+}
+
+func GetAddressAndDialer(endpoint string) (string, func(ctx context.Context, addr string) (net.Conn, error), error) {
+	protocol, addr, err := parseEndpointWithFallbackProtocol(endpoint, unixProtocol)
+	if err != nil {
+		return "", nil, err
+	}
+	if protocol != unixProtocol {
+		return "", nil, errOnlySupportUnixSocket
+	}
+
+	return addr, dial, nil
+}
+
+func dial(ctx context.Context, addr string) (net.Conn, error) {
+	return (&net.Dialer{}).DialContext(ctx, unixProtocol, addr)
+}
+
+func parseEndpointWithFallbackProtocol(endpoint string, fallbackProtocol string) (protocol string, addr string, err error) {
+	if protocol, addr, err = parseEndpoint(endpoint); err != nil && protocol == "" {
+		fallbackEndpoint := fallbackProtocol + "://" + endpoint
+		protocol, addr, err = parseEndpoint(fallbackEndpoint)
+		if err != nil {
+			return "", "", err
+		}
+	}
+	return protocol, addr, err
+}
+
+func parseEndpoint(endpoint string) (string, string, error) {
+	u, err := url.Parse(endpoint)
+	if err != nil {
+		return "", "", fmt.Errorf("error while parsing: %w", err)
+	}
+
+	switch u.Scheme {
+	case "tcp":
+		return "tcp", u.Host, nil
+	case "unix":
+		return "unix", u.Path, nil
+
+	case "":
+		return "", "", fmt.Errorf("using %q as %w", endpoint, errEndpointDeprecated)
+
+	default:
+		return u.Scheme, "", fmt.Errorf("%q: %w", u.Scheme, errProtocolNotSupported)
+	}
+}
+
+func getAllImages(c Client) ([]string, error) {
+	backgroundContext, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	images, err := c.listImages(backgroundContext)
+	if err != nil {
+		return nil, err
+	}
+
+	allImages := make([]string, 0, len(images))
+
+	for _, img := range images {
+		allImages = append(allImages, img.Id)
+	}
+
+	return allImages, nil
+}
+
+func main() {
+	fmt.Fprintln(os.Stderr, "TEST")
+	runtimePtr := flag.String("runtime", "containerd", "container runtime")
+
+	flag.Parse()
+
+	if err := logger.Configure(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error setting up logger:", err)
+		os.Exit(1)
+	}
+
+	var socketPath string
+
+	switch runtime := *runtimePtr; runtime {
+	case "docker":
+		socketPath = "unix:///var/run/dockershim.sock"
+	case "containerd":
+		socketPath = "unix:///run/containerd/containerd.sock"
+	case "cri-o":
+		socketPath = "unix:///var/run/crio/crio.sock"
+	default:
+		log.Error(fmt.Errorf("unsupported runtime"), "runtime", runtime)
+		os.Exit(1)
+	}
+
+	log.Info("RUNTIME: ", socketPath)
+
+	imageclient, conn, err := getImageClient(context.Background(), socketPath)
+	if err != nil {
+		log.Error(err, "failed to get image client")
+		os.Exit(1)
+	}
+
+	runTimeClient := pb.NewRuntimeServiceClient(conn)
+
+	client := &client{imageclient, runTimeClient}
+
+	ls, err := getAllImages(client)
+
+	if err != nil {
+		log.Error(err, "failed to list all images")
+		os.Exit(1)
+	}
+
+	log.Info("List of images on node:")
+	// append ls into ImageCollectorCR
+	for _, img := range ls {
+		fmt.Fprintln(os.Stderr, img)
+	}
+}
---
+++ pkg/collector/go.mod
@@ -0,0 +1,10 @@
+module github.com/Azure/eraser
+
+go 1.16
+
+require (
+	google.golang.org/grpc v1.46.0
+	k8s.io/cri-api v0.23.6
+	sigs.k8s.io/controller-runtime v0.11.2
+	github.com/go-logr/logr v1.2.3
+)
