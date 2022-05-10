e6a4f2091f6305679f780bc543cfc2ef0a854168
ashnamehrotra <ashnamehrotra@gmail.com>
2022-05-10 10:05:45 -0400

    outline imagecollector controller

--- main.go
+++ main.go
@@ -81,6 +81,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.ImageCollectorReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ImageCollector")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
