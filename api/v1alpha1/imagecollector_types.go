be73cdce8763ca1df7789fa14959daa5d9e8be5f
ashnamehrotra <ashnamehrotra@gmail.com>
2022-05-10 14:11:32 -0400

    Imagecollector crd (#171)
    * add image collector crd
    
    * save changes
    
    * change image collector CRD to store image name and node
    
    * change name string of names in case of multiple
    
    * remove node name and store image name as string
    
    * add imagecollector to kustomization file

---
+++ api/v1alpha1/imagecollector_types.go
@@ -0,0 +1,54 @@
+/*
+Copyright 2021.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type ImageCollectorSpec struct {
+	Images []Image `json:"images"`
+}
+
+type Image struct {
+	Digest string `json:"digest"`
+	Name   string `json:"name,omitempty"`
+}
+
+type ImageCollectorStatus struct {
+	Result []Image `json:"result"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+//+kubebuilder:resource:scope="Cluster"
+type ImageCollector struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ImageCollectorSpec   `json:"spec,omitempty"`
+	Status ImageCollectorStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+type ImageCollectorList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ImageCollector `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ImageCollector{}, &ImageCollectorList{})
+}
