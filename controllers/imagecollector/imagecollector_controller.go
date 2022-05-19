9904b5266bc212c39059f941ddcafb1d9708bd4c
ashnamehrotra <ashnamehrotra@gmail.com>
2022-05-19 11:21:21 -0400

    requeue after logic

--- controllers/imagecollector/imagecollector_controller.go
+++ controllers/imagecollector/imagecollector_controller.go
@@ -99,6 +99,7 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.2/pkg/reconcile
 func (r *ImageCollectorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log.Info("ImageCollector Reconcile")
 	// periodically create imageJob with collector pods
 	job := &eraserv1alpha1.ImageJob{
 		ObjectMeta: metav1.ObjectMeta{
