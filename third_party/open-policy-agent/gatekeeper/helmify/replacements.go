2fc8f1166d830f33dcc82a54786579953ffb5d80
Andrew Malkov <an.malkov@outlook.com>
2022-05-19 01:49:47 +0000

    apply PR comments to align helm values per component

--- third_party/open-policy-agent/gatekeeper/helmify/replacements.go
+++ third_party/open-policy-agent/gatekeeper/helmify/replacements.go
@@ -1,8 +1,8 @@
 package main
 
 var replacements = map[string]string{
-	`HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_CONTAINER_RESOURCES: ""`: `{{- toYaml .Values.resources | nindent 10 }}`,
-	`HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_NODESELECTOR: ""`: `{{- toYaml .Values.nodeSelector | nindent 8 }}`,
-	`HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_TOLERATIONS: ""`: `{{- toYaml .Values.tolerations | nindent 8 }}`,
-	`HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_AFFINITY: ""`: `{{- toYaml .Values.affinity | nindent 8 }}`,
+	`HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_CONTAINER_RESOURCES: ""`: `{{- toYaml .Values.controllerManager.resources | nindent 10 }}`,
+	`HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_NODESELECTOR: ""`:        `{{- toYaml .Values.controllerManager.nodeSelector | nindent 8 }}`,
+	`HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_TOLERATIONS: ""`:         `{{- toYaml .Values.controllerManager.tolerations | nindent 8 }}`,
+	`HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_AFFINITY: ""`:            `{{- toYaml .Values.controllerManager.affinity | nindent 8 }}`,
 }
