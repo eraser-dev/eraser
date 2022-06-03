3ecf9b631fc8f21b8d865f850c4ac6f77da51809
ashnamehrotra <ashnamehrotra@gmail.com>
2022-06-03 12:51:57 -0400

    remove scanner image argument flag, unused

--- controllers/imagecollector/imagecollector_controller.go
+++ controllers/imagecollector/imagecollector_controller.go
@@ -50,7 +50,6 @@ var (
 	collectorImage = flag.String("collector-image", "ghcr.io/azure/collector:latest", "collector image")
 	log            = logf.Log.WithName("controller").WithValues("process", "imagecollector-controller")
 	repeatPeriod   = flag.Duration("repeat-period", time.Hour*24, "repeat period for collec/scan process")
-	scannerImage   = flag.String("scanner-image", "ghcr.io/azure/scanner:latest", "scanner image")
 )
 
 const (
