41435fac4cd7209b3de7ba8a22d179b51b01ca6e
ashnamehrotra <ashnamehrotra@gmail.com>
2022-06-10 16:55:58 -0400

    change scanDisabled from var to method

--- controllers/imagecollector/imagecollector_controller.go
+++ controllers/imagecollector/imagecollector_controller.go
@@ -53,7 +53,6 @@ var (
 	log                    = logf.Log.WithName("controller").WithValues("process", "imagecollector-controller")
 	repeatPeriod           = flag.Duration("repeat-period", time.Hour*24, "repeat period for collect/scan process")
 	deleteScanFailedImages = flag.Bool("delete-scan-failed-images", true, "whether or not to delete images for which scanning has failed")
-	scanDisabled           = true
 )
 
 const (
@@ -172,10 +171,6 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log.Info("ImageCollector Reconcile")
 
-	if *scannerImage != "" {
-		scanDisabled = false
-	}
-
 	imageCollectorShared := &eraserv1alpha1.ImageCollector{
 		TypeMeta:   metav1.TypeMeta{Kind: "ImageCollector", APIVersion: apiVersion},
 		ObjectMeta: metav1.ObjectMeta{Name: collectorShared},
@@ -252,7 +247,7 @@ func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 		}
 
 		// if scan is disabled, create/update imagelist to prune since collector job has finished
-		if scanDisabled {
+		if scanDisabled() {
 			if res, err := r.upsertImageList(ctx, imageCollectorShared); err != nil {
 				return res, err
 			}
@@ -310,7 +305,7 @@ func (r *Reconciler) upsertImageList(ctx context.Context, collector *eraserv1alp
 	imageListItems := make([]string, 0, len(collector.Spec.Images))
 
 	images := collector.Spec.Images
-	if !scanDisabled {
+	if !scanDisabled() {
 		images = collector.Status.Vulnerable
 
 		if *deleteScanFailedImages {
@@ -339,6 +334,10 @@ func (r *Reconciler) upsertImageList(ctx context.Context, collector *eraserv1alp
 	}
 
 	return ctrl.Result{}, nil
+}
+
+func scanDisabled() bool {
+	return *scannerImage == ""
 }
 
 func (r *Reconciler) createImageList(ctx context.Context, collector *eraserv1alpha1.ImageCollector, items []string) (ctrl.Result, error) {
