a9b59edf30afbfb643dfddb40a845ae336aba08c
Xander Grzywinski <xgrzywinski@microsoft.com>
2022-06-21 10:15:15 -0700

    add needed gitignore items

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -12,6 +12,7 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
