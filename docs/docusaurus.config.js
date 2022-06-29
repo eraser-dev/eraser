9f08118def711dc4f015e622e32e8895c50c7a8b
Xander Grzywinski <xgrzywinski@microsoft.com>
2022-06-29 13:02:39 -0700

    revert testing changes

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Eraser Docs',
-  url: 'https://salaxander.github.io/eraser',
+  url: 'https://azure.github.io/eraser',
   baseUrl: '/eraser/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
