635e43bb2e465bded0c8b32e85e8e0f6c543b0de
Xander Grzywinski <xgrzywinski@microsoft.com>
2022-06-29 13:40:17 -0700

    docs: docusaurus docs site (#258)
    * docusaurus site
    
    * add needed gitignore items
    
    * add GitHub action to deploy docs
    
    * docs: update docs for v0.2.0
    
    * docs: remove blog and updated gitignore
    
    * PR comments
    
    * update workflow
    
    * docs: remove page scaffold
    
    * docs: add destination dir
    
    * docs: remove duplicated helm doc and reference existing
    
    * docs: change azure logo to broom
    
    * docs: remove extra static assets
    
    * docs: respond to PR comments
    
    * test action
    
    * remove yarn
    
    * change URL
    
    * baseURL
    
    * revert testing changes
    
    * remove test file
    
    * remove old lockfile
    
    * remove yarn lock from gitignore
    
    * yarn audit fix
    
    * remove audit fix package

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,76 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Eraser Docs',
+  url: 'https://azure.github.io/eraser',
+  baseUrl: '/eraser/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  trailingSlash: false,
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Azure', // Usually your GitHub org/user name.
+  projectName: 'Eraser', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/'
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Eraser',
+        logo: {
+          alt: 'Eraser Logo',
+          src: 'img/broom.png',
+        },
+        items: [
+          {
+            href: 'https://github.com/azure/eraser',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Microsoft. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
