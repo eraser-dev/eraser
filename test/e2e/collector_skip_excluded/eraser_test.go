f90ab247254a1a2f7f11eabdad93ec02477d0444
ashnamehrotra <ashnamehrotra@gmail.com>
2022-07-01 16:21:32 -0400

    remove duplicate

--- test/e2e/collector_skip_excluded/eraser_test.go
+++ test/e2e/collector_skip_excluded/eraser_test.go
@@ -134,27 +134,6 @@ func TestCollectorExcluded(t *testing.T) {
 
 			return ctx
 		}).
-		Assess("Pods from imagejobs are cleaned up", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			c, err := cfg.NewClient()
-			if err != nil {
-				t.Fatal("Failed to create new client", err)
-			}
-
-			var ls corev1.PodList
-			err = c.Resources().List(ctx, &ls, func(o *metav1.ListOptions) {
-				o.LabelSelector = labels.SelectorFromSet(map[string]string{"name": "collector"}).String()
-			})
-			if err != nil {
-				t.Errorf("could not list pods: %v", err)
-			}
-
-			err = wait.For(conditions.New(c.Resources()).ResourcesDeleted(&ls), wait.WithTimeout(time.Minute))
-			if err != nil {
-				t.Errorf("error waiting for pods to be deleted: %v", err)
-			}
-
-			return ctx
-		}).
 		Feature()
 
 	util.Testenv.Test(t, collectorExcluded)
