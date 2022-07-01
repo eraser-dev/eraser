f5dc77626e5f9319043dbd23a9b90f1155e6a4bc
ashnamehrotra <ashnamehrotra@gmail.com>
2022-07-01 15:19:38 -0400

    get imagecollector-shared and check for alpine

--- test/e2e/collector_skip_excluded/eraser_test.go
+++ test/e2e/collector_skip_excluded/eraser_test.go
@@ -58,6 +58,7 @@ func TestCollectorExcluded(t *testing.T) {
 			return ctx
 		}).
 		Assess("ImageList CR is generated", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			// check that imagelist CR is generated to make sure collection portion is completed
 			c, err := cfg.NewClient()
 			if err != nil {
 				t.Fatal("Failed to create new client", err)
@@ -83,22 +84,34 @@ func TestCollectorExcluded(t *testing.T) {
 
 			return ctx
 		}).
-		Assess("ImageList CR shared does not contain Alpine", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			resource := eraserv1alpha1.ImageCollector{}
-			err := c.Resources().Get(ctx, util.ImageCollectorShared, "default", &resource)
+		Assess("ImageCollector CR shared does not contain Alpine", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			c, err := cfg.NewClient()
 			if err != nil {
-				return false, err
+				t.Fatal("Failed to create new client", err)
 			}
+
+			resource := eraserv1alpha1.ImageCollector{}
+			wait.For(func() (bool, error) {
+				err := c.Resources().Get(ctx, util.ImageCollectorShared, "default", &resource)
+				if err != nil {
+					return false, err
+				}
+
+				if resource.ObjectMeta.Name == util.ImageCollectorShared {
+					return true, nil
+				}
+
+				return false, nil
+			}, wait.WithTimeout(time.Minute*3))
 
 			// alpine is excluded and should not be added to imagecollector-shared
 			for _, img := range resource.Spec.Images {
-				if strings.Contains("docker.io/library/alpine", img) {
-					return false, nil
+				if strings.Contains("docker.io/library/alpine", img.Name) {
+					t.Error("imagecollector-shared should not contains alpine", img.Name)
 				}
 			}
 
 			return ctx
-
 		}).
 		Assess("Pods from imagejobs are cleaned up", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
 			c, err := cfg.NewClient()
