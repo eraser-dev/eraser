1f03406635d3f46dc7aaa05d8a77b8e7614a2a57
ashnamehrotra <ashnamehrotra@gmail.com>
2022-07-11 15:55:55 -0400

    add init declaration and flag variable for eraserArgs

--- controllers/imagelist/imagelist_controller.go
+++ controllers/imagelist/imagelist_controller.go
@@ -55,7 +55,12 @@ var (
 	log         = logf.Log.WithName("controller").WithValues("process", "imagelist-controller")
 	eraserImage = flag.String("eraser-image", "ghcr.io/azure/eraser:latest", "eraser image")
 	imageList   = types.NamespacedName{Name: "imagelist"}
+	eraserArgs  = utils.MultiFlag([]string{})
 )
+
+func init() {
+	flag.Var(&eraserArgs, "eraser-arg", "An argument to be passed through to the eraser. For example, --eraser-arg=--enable-pprof=true will pass through to the eraser as --enable-pprof=true. Can be supplied multiple times.")
+}
 
 func Add(mgr manager.Manager) error {
 	return add(mgr, newReconciler(mgr))
