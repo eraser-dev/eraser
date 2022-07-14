c2c8dec8bbf1324f23fd75bf6c5c8b28725a3949
ashnamehrotra <ashnamehrotra@gmail.com>
2022-07-14 17:40:39 -0400

    fix: cleanup service account permissions (#324)
    * cleanup service account permissions
    
    * remove duplicates and finalizers

--- controllers/imagecollector/imagecollector_controller.go
+++ controllers/imagecollector/imagecollector_controller.go
@@ -163,10 +163,8 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 }
 
 //+kubebuilder:rbac:groups=eraser.sh,resources=imagecollectors,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=eraser.sh,resources=imagelists,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=eraser.sh,resources=imagecollectors/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=eraser.sh,resources=imagecollectors/finalizers,verbs=update
-//+kubebuilder:rbac:groups="batch",resources=jobs,verbs=get;list;watch;update;create;delete
+//+kubebuilder:rbac:groups="batch",resources=jobs,verbs=get;list;create;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
