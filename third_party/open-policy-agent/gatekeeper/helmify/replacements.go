310f1119ae03e4380dd0e991ced59eb1829921f5
ashnamehrotra <ashnamehrotra@gmail.com>
2022-07-14 16:12:58 -0400

    fix: move additional args (#326)
    * move additional args outside of controller iimage
    
    * modify syntax text
    
    * update manifest_staging

--- third_party/open-policy-agent/gatekeeper/helmify/replacements.go
+++ third_party/open-policy-agent/gatekeeper/helmify/replacements.go
@@ -8,5 +8,5 @@ var replacements = map[string]string{
 	`- HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_SCANNER_ARGS`:          `{{- if .Values.scanner.image.args }}{{- range .Values.scanner.image.args }}{{ nindent 8 "- --scanner-arg=" }}{{ . }}{{- end -}}{{ end }}`,
 	`- HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_ERASER_ARGS`:           `{{- if .Values.eraser.image.args }}{{- range .Values.eraser.image.args }}{{ nindent 8 "- --eraser-arg=" }}{{ . }}{{- end -}}{{ end }}`,
 	`- HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_COLLECTOR_ARGS`:        `{{- if .Values.collector.image.args }}{{- range .Values.collector.image.args }}{{ nindent 8 "- --collector-arg=" }}{{ . }}{{- end -}}{{ end }}`,
-	`- HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_ADDITIONAL_ARGS`:       `{{- if .Values.controllerManager.image.additionalArgs }}{{- range .Values.controllerManager.image.additionalArgs }}{{ nindent 8 "- " }}{{ . }}{{- end -}}{{ end }}`,
+	`- HELMSUBST_DEPLOYMENT_CONTROLLER_MANAGER_ADDITIONAL_ARGS`:       `{{- if .Values.controllerManager.additionalArgs }}{{- range .Values.controllerManager.additionalArgs }}{{ nindent 8 "- " }}{{ . }}{{- end -}}{{ end }}`,
 }
