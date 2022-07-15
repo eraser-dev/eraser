9160d84bf144c3dd4e6fb706ce7651d816ce4c64
ashnamehrotra <ashnamehrotra@gmail.com>
2022-07-15 16:55:14 -0400

    fix: cleanup sa, add watch permissions (#329)
    * cleanup service account permissions
    
    * remove duplicates and finalizers
    
    * add watch nodes
    
    * add watch jobs
    
    * make manifest

--- controllers/imagecollector/imagecollector_controller.go
+++ controllers/imagecollector/imagecollector_controller.go
@@ -164,7 +164,7 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 
 //+kubebuilder:rbac:groups=eraser.sh,resources=imagecollectors,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=eraser.sh,resources=imagecollectors/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups="batch",resources=jobs,verbs=get;list;create;delete
+//+kubebuilder:rbac:groups="batch",resources=jobs,verbs=get;list;create;delete;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
