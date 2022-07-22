bd87fe82c6eb7e0551e90c1ac93741cc79697ee8
ashnamehrotra <ashnamehrotra@gmail.com>
2022-07-22 12:20:28 -0400

    fix mkfifo commands into one argument

--- controllers/imagecollector/imagecollector_controller.go
+++ controllers/imagecollector/imagecollector_controller.go
@@ -455,7 +455,7 @@ func (r *Reconciler) createImageJob(ctx context.Context, req ctrl.Request, image
 								{MountPath: "/run/eraser.sh/shared-data", Name: "shared-data"},
 							},
 							Command: []string{"/bin/sh", "-c"},
-							Args:    []string{"mkfifo /run/eraser.sh/shared-data/collectScan", "mkfifo /run/eraser.sh/shared-data/scanErase"},
+							Args:    []string{"mkfifo /run/eraser.sh/shared-data/collectScan; mkfifo /run/eraser.sh/shared-data/scanErase"},
 						},
 					},
 					Containers: []corev1.Container{
