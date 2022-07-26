1d02973e43992e9c78a4f16e9885ea8e2a950a84
ashnamehrotra <ashnamehrotra@gmail.com>
2022-07-26 13:14:05 -0400

    modify scan disable test: check that a non vulnerable image (nginx) was deleted instead of referencing imagecollector-shared

--- test/e2e/tests/collector_disable_scan/eraser_test.go
+++ test/e2e/tests/collector_disable_scan/eraser_test.go
@@ -18,75 +18,88 @@ import (
 
 func TestDisableScanner(t *testing.T) {
 	disableScanFeat := features.New("Scanner can be disabled").
-		Assess("ImageCollector CR is generated", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			c, err := cfg.NewClient()
+		Setup(func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			podSelectorLabels := map[string]string{"app": util.Nginx}
+			nginxDep := util.NewDeployment(cfg.Namespace(), util.Nginx, 2, podSelectorLabels, corev1.Container{Image: util.Nginx, Name: util.Nginx})
+			if err := cfg.Client().Resources().Create(ctx, nginxDep); err != nil {
+				t.Error("Failed to create the dep", err)
+			}
+			return ctx
+		}).
+		Assess("deployment successfully deployed", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			client, err := cfg.NewClient()
 			if err != nil {
 				t.Error("Failed to create new client", err)
 			}
 
-			imagecollector := eraserv1alpha1.ImageCollector{}
-			wait.For(func() (bool, error) {
-				err := c.Resources().Get(ctx, util.ImageCollectorShared, "default", &imagecollector)
+			resultDeployment := appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{Name: util.Nginx, Namespace: cfg.Namespace()},
+			}
+
+			if err = wait.For(conditions.New(client.Resources()).DeploymentConditionMatch(&resultDeployment, appsv1.DeploymentAvailable, corev1.ConditionTrue),
+				wait.WithTimeout(time.Minute*3)); err != nil {
+				t.Error("deployment not found", err)
+			}
+
+			return context.WithValue(ctx, util.Nginx, &resultDeployment)
+		}).
+		Assess("Images successfully deleted from all nodes", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			// delete deployment
+			client, err := cfg.NewClient()
+			if err != nil {
+				t.Error("Failed to create new client", err)
+			}
+
+			var pods corev1.PodList
+			err = client.Resources().List(ctx, &pods, func(o *metav1.ListOptions) {
+				o.LabelSelector = labels.SelectorFromSet(labels.Set{"app": util.Nginx}).String()
+			})
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			dep := ctx.Value(util.Nginx).(*appsv1.Deployment)
+			if err := client.Resources().Delete(ctx, dep); err != nil {
+				t.Error("Failed to delete the dep", err)
+			}
+
+			for _, nodeName := range util.GetClusterNodes(t) {
+				err := wait.For(util.ContainerNotPresentOnNode(nodeName, util.Nginx), wait.WithTimeout(time.Minute*2))
 				if err != nil {
-					t.Error("Could not get imagecollector-shared")
+					// Let's not mark this as an error
+					// We only have this to prevent race conditions with the eraser spinning up
+					t.Logf("error while waiting for deployment deletion: %v", err)
 				}
+			}
 
-				if imagecollector.ObjectMeta.Name == util.ImageCollectorShared {
-					return true, nil
-				}
+			// deploy imageJob config
+			if err := util.DeployEraserConfig(cfg.KubeconfigFile(), util.EraserNamespace, "../../test-data", "eraser_v1alpha1_imagelist.yaml"); err != nil {
+				t.Error("Failed to deploy image list config", err)
+			}
 
-				return false, nil
-			}, wait.WithTimeout(time.Minute*3))
+			ctxT, cancel := context.WithTimeout(ctx, time.Minute)
+			defer cancel()
+			util.CheckImageRemoved(ctxT, t, util.GetClusterNodes(t), util.Nginx)
 
 			return ctx
 		}).
-		Assess("ImageList Spec Contains Same Images As ImageCollector Shared", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+		Assess("Pods from imagejobs are cleaned up", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
 			c, err := cfg.NewClient()
 			if err != nil {
 				t.Error("Failed to create new client", err)
 			}
 
-			// verify imagelist created
-			imagelist := eraserv1alpha1.ImageList{}
-			wait.For(func() (bool, error) {
-				err := c.Resources().Get(ctx, "imagelist", "default", &imagelist)
-				if util.IsNotFound(err) {
-					return false, nil
-				}
-
-				if err != nil {
-					return false, err
-				}
-
-				if imagelist.ObjectMeta.Name == "imagelist" {
-					return true, nil
-				}
-
-				return false, nil
-			}, wait.WithTimeout(time.Minute*3))
-
-			imagecollectorShared := eraserv1alpha1.ImageCollector{}
-			err = c.Resources().Get(ctx, util.ImageCollectorShared, "default", &imagecollectorShared)
+			var ls corev1.PodList
+			err = c.Resources().List(ctx, &ls, func(o *metav1.ListOptions) {
+				o.LabelSelector = labels.SelectorFromSet(map[string]string{"name": "eraser"}).String()
+			})
 			if err != nil {
-				t.Error("Could not get imagecollector-shared")
+				t.Errorf("could not list pods: %v", err)
 			}
 
-			// verify imagecollector-shared status fields are empty
-			if imagecollectorShared.Status.Vulnerable != nil || imagecollectorShared.Status.Failed != nil {
-				t.Error("Scan job has run, should be disabled")
-			}
-
-			imagelistSpec := make(map[string]struct{}, len(imagelist.Spec.Images))
-			for _, img := range imagelist.Spec.Images {
-				imagelistSpec[img] = struct{}{}
-			}
-
-			// verify the images in both lists match
-			for _, img := range imagecollectorShared.Spec.Images {
-				// check by digest as we add to imagelist by digest when pruning without scanner
-				if _, contains := imagelistSpec[img.Digest]; !contains {
-					t.Error("imagelist spec does not match imagecollector-shared: ", img.Digest)
-				}
+			err = wait.For(conditions.New(c.Resources()).ResourcesDeleted(&ls), wait.WithTimeout(time.Minute))
+			if err != nil {
+				t.Errorf("error waiting for pods to be deleted: %v", err)
 			}
 
 			return ctx
