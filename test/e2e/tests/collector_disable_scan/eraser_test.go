ca5877b745c65a5f95b0a7d4a0137b3d43bdf911
ashnamehrotra <ashnamehrotra@gmail.com>
2022-07-28 11:53:12 -0400

    replace nginx with nonvulnerable image in scan disabled test

--- test/e2e/tests/collector_disable_scan/eraser_test.go
+++ test/e2e/tests/collector_disable_scan/eraser_test.go
@@ -21,11 +21,11 @@ import (
 
 func TestDisableScanner(t *testing.T) {
 	disableScanFeat := features.New("Scanner can be disabled").
-		// nginx is not a vulnerable image, so it should be deleted from all nodes when scanner is disabled and we prune with collector
-		Assess("Nginx successfully deleted from all nodes", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+		// non-vulnerable image should be deleted from all nodes when scanner is disabled and we prune with collector
+		Assess("Non-vulnerable image successfully deleted from all nodes", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
 			ctxT, cancel := context.WithTimeout(ctx, time.Minute*5)
 			defer cancel()
-			util.CheckImageRemoved(ctxT, t, util.GetClusterNodes(t), util.Nginx)
+			util.CheckImageRemoved(ctxT, t, util.GetClusterNodes(t), util.NonVulnerableImage)
 
 			return ctx
 		}).
