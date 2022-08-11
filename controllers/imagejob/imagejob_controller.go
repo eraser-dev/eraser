2833120e018d94d8858694777af9ee311d13906c
ashnamehrotra <ashnamehrotra@gmail.com>
2022-08-11 16:41:11 -0400

    remove create and watch configmap role

--- controllers/imagejob/imagejob_controller.go
+++ controllers/imagejob/imagejob_controller.go
@@ -144,7 +144,7 @@ func checkNodeFitness(pod *corev1.Pod, node *corev1.Node) bool {
 
 //+kubebuilder:rbac:groups=eraser.sh,resources=imagejobs,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=eraser.sh,resources=imagejobs/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups="",resources=configmaps,verbs=create;get;list;watch
+//+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
