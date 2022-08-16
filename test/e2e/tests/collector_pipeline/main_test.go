add2d9df0fa14c5b74339fdeb0c5f5f8e047b6bf
ashnamehrotra <ashnamehrotra@gmail.com>
2022-08-16 16:34:52 -0400

    delete new test

--- test/e2e/tests/collector_pipeline/main_test.go
+++ test/e2e/tests/collector_pipeline/main_test.go
@@ -29,7 +29,7 @@ func TestMain(m *testing.M) {
 		envfuncs.LoadDockerImageToCluster(util.KindClusterName, util.ScannerImage),
 		envfuncs.LoadDockerImageToCluster(util.KindClusterName, util.CollectorImage),
 		envfuncs.LoadDockerImageToCluster(util.KindClusterName, util.VulnerableImage),
-		util.DeployEraserManifest(util.EraserNamespace),
+		util.DeployEraserNonHelm(util.EraserNamespace, "../../../../manifest_staging/deploy", "eraser.yaml"),
 	).Finish(
 		envfuncs.DestroyKindCluster(util.KindClusterName),
 	)
