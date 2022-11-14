af3b899b3cc9b13e7b4d4e7e96ecd7ee52f46f91
Peter Engelbert <pmengelbert@gmail.com>
2022-11-14 16:36:54 -0500

    save
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -116,6 +116,23 @@ func ListImages(ctx context.Context, images pb.ImageServiceClient) (list []*pb.I
 		return nil, err
 	}
 
+	for _, img := range resp.Images {
+		ximg := pb.ImageSpec{Image: img.Id}
+		req := pb.ImageStatusRequest{
+			Image:   &ximg,
+			Verbose: true,
+		}
+
+		resp, err := images.ImageStatus(ctx, &req)
+		if err != nil {
+			return nil, err
+		}
+
+		fmt.Printf(`{ "IMAGE": "%#v",
+            "INFO": "%#v }"`, resp.Image, resp.Info)
+		_ = img
+	}
+
 	return resp.Images, nil
 }
 
@@ -241,6 +258,7 @@ func IsExcluded(excluded map[string]struct{}, img string, idToImageMap map[strin
 				}
 			}
 		}
+
 	}
 
 	return false
