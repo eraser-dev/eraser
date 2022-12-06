299572ecb2d2ba1dce8295710e4c2d9ddca6251e
ashnamehrotra <ashnamehrotra@gmail.com>
2022-12-06 12:44:28 -0500

    change controller-runtime metrics endpoint to 8889

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ import (
 var (
 	scheme               = runtime.NewScheme()
 	setupLog             = ctrl.Log.WithName("setup")
-	metricsAddr          = flag.String("metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	metricsAddr          = flag.String("metrics-bind-address", ":8889", "The address the metric endpoint binds to.")
 	probeAddr            = flag.String("health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	enableLeaderElection = flag.Bool("leader-elect", false,
 		"Enable leader election for controller manager. "+
