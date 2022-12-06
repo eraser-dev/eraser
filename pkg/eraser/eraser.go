300059e59135103b36f1d76062e17ef4a1eef4c7
ashnamehrotra <ashnamehrotra@gmail.com>
2022-12-06 12:45:56 -0500

    move metrics before write to pipe

--- pkg/eraser/eraser.go
+++ pkg/eraser/eraser.go
@@ -139,6 +139,21 @@ func main() {
 		os.Exit(generalErr)
 	}
 
+	if os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT") != "" {
+		// record metrics
+		ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+		defer cancel()
+
+		exporter, reader, provider := metrics.ConfigureMetrics(ctx, log, os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT"))
+		global.SetMeterProvider(provider)
+
+		defer metrics.ExportMetrics(log, exporter, reader, provider)
+
+		if err := metrics.RecordMetricsEraser(ctx, global.MeterProvider(), int64(removed)); err != nil {
+			log.Error(err, "error recording metrics")
+		}
+	}
+
 	if *imageListPtr == "" {
 		file, err := os.OpenFile(util.EraseCompleteCollectPath, os.O_WRONLY, 0)
 		if err != nil {
@@ -169,20 +184,5 @@ func main() {
 		}
 
 		file.Close()
-	}
-
-	if os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT") != "" {
-		// record metrics
-		ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-		defer cancel()
-
-		exporter, reader, provider := metrics.ConfigureMetrics(ctx, log, os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT"))
-		global.SetMeterProvider(provider)
-
-		defer metrics.ExportMetrics(log, exporter, reader, provider)
-
-		if err := metrics.RecordMetricsEraser(ctx, global.MeterProvider(), int64(removed)); err != nil {
-			log.Error(err, "error recording metrics")
-		}
 	}
 }
