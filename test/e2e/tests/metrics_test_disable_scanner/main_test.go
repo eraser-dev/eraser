69c92ceaed732a09df359411553157f0dcc5a178
ashnamehrotra <ashnamehrotra@gmail.com>
2022-12-13 11:47:51 -0500

    fix: metrics fixes (#510)
    * change controller-runtime metrics endpoint to 8889
    
    * move metrics before write to pipe
    
    * lint fix, removed defer for export metrics
    
    * lint fix, move cancel()
    
    * add test with scanner disabled
    
    * add logs to metrics tests
    
    * fix test namespaces

---
+++ test/e2e/tests/metrics_test_disable_scanner/main_test.go
@@ -0,0 +1,49 @@
+//go:build e2e
+// +build e2e
+
+package e2e
+
+import (
+	"os"
+	"testing"
+
+	eraserv1alpha1 "github.com/Azure/eraser/api/v1alpha1"
+	"github.com/Azure/eraser/test/e2e/util"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/e2e-framework/pkg/env"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+	"sigs.k8s.io/e2e-framework/pkg/envfuncs"
+)
+
+func TestMain(m *testing.M) {
+	utilruntime.Must(eraserv1alpha1.AddToScheme(scheme.Scheme))
+
+	eraserImage := util.ParsedImages.EraserImage
+	managerImage := util.ParsedImages.ManagerImage
+	collectorImg := util.ParsedImages.CollectorImage
+
+	util.Testenv = env.NewWithConfig(envconf.New())
+	// Create KinD Cluster
+	util.Testenv.Setup(
+		envfuncs.CreateKindClusterWithConfig(util.KindClusterName, util.NodeVersion, "../../kind-config.yaml"),
+		envfuncs.CreateNamespace(util.TestNamespace),
+		util.DeployOtelCollector(util.TestNamespace),
+		envfuncs.LoadDockerImageToCluster(util.KindClusterName, util.ManagerImage),
+		envfuncs.LoadDockerImageToCluster(util.KindClusterName, util.CollectorImage),
+		envfuncs.LoadDockerImageToCluster(util.KindClusterName, util.Image),
+		envfuncs.LoadDockerImageToCluster(util.KindClusterName, util.VulnerableImage),
+		util.DeployEraserHelm(util.TestNamespace,
+			"--set", util.ScannerImageRepo.Set(""),
+			"--set", util.CollectorImageRepo.Set(collectorImg.Repo),
+			"--set", util.CollectorImageTag.Set(collectorImg.Tag),
+			"--set", util.EraserImageRepo.Set(eraserImage.Repo),
+			"--set", util.EraserImageTag.Set(eraserImage.Tag),
+			"--set", util.ManagerImageRepo.Set(managerImage.Repo),
+			"--set", util.ManagerImageTag.Set(managerImage.Tag),
+			"--set", `controllerManager.additionalArgs={--job-cleanup-on-success-delay=1m,--otlp-endpoint=otel-collector:4318}`),
+	).Finish(
+		envfuncs.DestroyKindCluster(util.KindClusterName),
+	)
+	os.Exit(util.Testenv.Run(m))
+}
--- test/e2e/tests/metrics_test_scanner/eraser_test.go
+++ test/e2e/tests/metrics_test_scanner/eraser_test.go
@@ -28,6 +28,17 @@ func TestMetrics(t *testing.T) {
 
 			return ctx
 		}).
+		Assess("Get logs", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			if err := util.GetPodLogs(ctx, cfg, t, false); err != nil {
+				t.Error("error getting collector pod logs", err)
+			}
+
+			if err := util.GetManagerLogs(ctx, cfg, t); err != nil {
+				t.Error("error getting manager logs", err)
+			}
+
+			return ctx
+		}).
 		Assess("Check images_removed_run_total metric", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
 			if _, err := util.KubectlCurlPod(cfg.KubeconfigFile(), cfg.Namespace()); err != nil {
 				t.Error(err, "error running curl pod")
