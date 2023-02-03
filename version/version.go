56d24081b302907ad2e5322295c6e7058580237e
Peter Engelbert <pmengelbert@gmail.com>
2023-02-03 16:43:21 -0500

    Re-generate manifests
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- version/version.go
+++ version/version.go
@@ -8,16 +8,15 @@ import (
 var (
 	// BuildVersion is version set on build.
 	BuildVersion string
+	// DefaultRepo is the default repo for images.
+	DefaultRepo = "ghcr.io/azure"
 	// buildTime is the date for the binary build.
 	buildTime string
 	// vcsCommit is the commit hash for the binary build.
 	vcsCommit string
 )
 
-var (
-	// DefaultRepo is the default repo for images.
-	DefaultRepo = "ghcr.io/azure"
-)
+var ()
 
 // GetUserAgent returns a user agent of the format eraser/<component>/<version> (<goos>/<goarch>) <commit>/<timestamp>.
 func GetUserAgent(component string) string {
