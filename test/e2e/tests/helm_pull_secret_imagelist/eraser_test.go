d632676e0884bd0b91aa92f49a42172a86fc5e72
Peter Engelbert <pmengelbert@gmail.com>
2023-02-28 15:37:14 -0500

    Fix log retrieval
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- test/e2e/tests/helm_pull_secret_imagelist/eraser_test.go
+++ test/e2e/tests/helm_pull_secret_imagelist/eraser_test.go
@@ -87,7 +87,7 @@ func TestHelmPullSecret(t *testing.T) {
 			return ctx
 		}).
 		Assess("Get logs", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			if err := util.GetPodLogs(ctx, cfg, t, false); err != nil {
+			if err := util.GetPodLogs(ctx, cfg, t, true); err != nil {
 				t.Error("error getting collector pod logs", err)
 			}
 
