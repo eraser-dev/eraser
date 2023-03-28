530ce4e457e458197759d3733e0a5e55beae12e9
ashnamehrotra <ashnamehrotra@gmail.com>
2023-03-28 19:24:03 +0000

    regenerate demo using kind cluster name

--- demo/demo.sh
+++ demo/demo.sh
@@ -6,7 +6,7 @@
 . demo-magic.sh
 
 # boostrap environment
-pei "kind create cluster"
+pei "kind create cluster --name eraser-demo"
 sleep 10
 pei "kubectl apply -f ds.yaml"
 sleep 10
@@ -18,13 +18,13 @@ sleep 10
 pei "kubectl delete daemonset alpine"
 sleep 5
 pei "kubectl get pods"
-pei "docker exec kind-control-plane ctr -n k8s.io images list | grep alpine"
+pei "docker exec eraser-demo-control-plane ctr -n k8s.io images list | grep alpine"
 pei "helm install -n eraser-system eraser eraser/eraser --create-namespace"
-sleep 5
+wait
 pei "kubectl get po -n eraser-system"
-sleep 60
+wait
 pei "kubectl get po -n eraser-system"
-pei "docker exec kind-control-plane ctr -n k8s.io images list | grep alpine"
+pei "docker exec eraser-demo-control-plane ctr -n k8s.io images list | grep alpine"
 
 # teardown environment
-kind delete cluster
+kind delete cluster --name eraser-demo
