e41b67574d67cd6b1d694ad3acbcd3ad36a29a76
ashnamehrotra <ashnamehrotra@gmail.com>
2023-06-09 19:43:21 +0000

    merge log into single line

--- pkg/scanners/trivy/trivy.go
+++ pkg/scanners/trivy/trivy.go
@@ -163,8 +163,7 @@ func main() {
 		log.Error(err, "total image scan timed out")
 	}
 
-	log.Info("Total number of vulnerabilities found", "Vuln Count", len(vulnerableImages))
-	log.Info("Vulnerable", "Images", vulnerableImages)
+	log.Info("Vulnerable", "Images", vulnerableImages, "Total count", len(vulnerableImages))
 
 	if len(failedImages) > 0 {
 		log.Info("Failed", "Images", failedImages)
