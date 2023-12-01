97fa9224a6836e7da6b78b799e115752d3ad8b2c
ashnamehrotra <ashnamehrotra@gmail.com>
2023-11-30 16:12:34 -0800

    modify unversioned_RuntimeSpec to unversioned_Runtime for this PR
    Signed-off-by: ashnamehrotra <ashnamehrotra@gmail.com>

--- api/v1alpha3/zz_generated.conversion.go
+++ api/v1alpha3/zz_generated.conversion.go
@@ -312,7 +312,7 @@ func Convert_unversioned_ImageJobConfig_To_v1alpha3_ImageJobConfig(in *unversion
 
 func autoConvert_v1alpha3_ManagerConfig_To_unversioned_ManagerConfig(in *ManagerConfig, out *unversioned.ManagerConfig, s conversion.Scope) error {
 	// WARNING: in.Runtime requires manual conversion: inconvertible types (./api/v1alpha3.RuntimeSpec vs github.com/eraser-dev/eraser/api/unversioned.Runtime)
-	if err := Convert_v1alpha3_RuntimeSpec_To_unversioned_RuntimeSpec(&in.Runtime, &out.Runtime, s); err != nil {
+	if err := Convert_v1alpha3_RuntimeSpec_To_unversioned_Runtime(&in.Runtime, &out.Runtime, s); err != nil {
 		return err
 	}
 	out.OTLPEndpoint = in.OTLPEndpoint
@@ -336,7 +336,7 @@ func autoConvert_v1alpha3_ManagerConfig_To_unversioned_ManagerConfig(in *Manager
 
 func autoConvert_unversioned_ManagerConfig_To_v1alpha3_ManagerConfig(in *unversioned.ManagerConfig, out *ManagerConfig, s conversion.Scope) error {
 	// WARNING: in.Runtime requires manual conversion: inconvertible types (github.com/eraser-dev/eraser/api/unversioned.Runtime vs ./api/v1alpha3.RuntimeSpec)
-	if err := Convert_unversioned_RuntimeSpec_To_v1alpha3_RuntimeSpec(&in.Runtime, &out.Runtime, s); err != nil {
+	if err := Convert_unversioned_Runtime_To_v1alpha3_RuntimeSpec(&in.Runtime, &out.Runtime, s); err != nil {
 		return err
 	}
 	out.OTLPEndpoint = in.OTLPEndpoint
@@ -501,13 +501,13 @@ func Convert_v1alpha3_ManagerConfig_To_unversioned_ManagerConfig(in *ManagerConf
 }
 
 // TODO: change this to use unversioned.RuntimeSpec when unversioned is updated
-func autoConvert_v1alpha3_RuntimeSpec_To_unversioned_RuntimeSpec(in *RuntimeSpec, out *unversioned.Runtime, s conversion.Scope) error {
+func autoConvert_v1alpha3_RuntimeSpec_To_unversioned_Runtime(in *RuntimeSpec, out *unversioned.Runtime, s conversion.Scope) error {
 	*out = unversioned.Runtime(in.Name)
 	return nil
 }
 
-func Convert_v1alpha3_RuntimeSpec_To_unversioned_RuntimeSpec(in *RuntimeSpec, out *unversioned.Runtime, s conversion.Scope) error {
-	return autoConvert_v1alpha3_RuntimeSpec_To_unversioned_RuntimeSpec(in, out, s)
+func Convert_v1alpha3_RuntimeSpec_To_unversioned_Runtime(in *RuntimeSpec, out *unversioned.Runtime, s conversion.Scope) error {
+	return autoConvert_v1alpha3_RuntimeSpec_To_unversioned_Runtime(in, out, s)
 }
 
 func Convert_unversioned_ManagerConfig_To_v1alpha3_ManagerConfig(in *unversioned.ManagerConfig, out *ManagerConfig, s conversion.Scope) error{
@@ -515,12 +515,12 @@ func Convert_unversioned_ManagerConfig_To_v1alpha3_ManagerConfig(in *unversioned
 }
 
 // TODO: change this to use unversioned.RuntimeSpec when unversioned is updated
-func autoConvert_unversioned_RuntimeSpec_To_v1alpha3_RuntimeSpec(in *unversioned.Runtime, out *RuntimeSpec, s conversion.Scope) error {
+func autoConvert_unversioned_Runtime_To_v1alpha3_RuntimeSpec(in *unversioned.Runtime, out *RuntimeSpec, s conversion.Scope) error {
 	out.Name = Runtime(string(*in))
 	out.Address = RuntimeAddress("")
 	return nil
 }
 
-func Convert_unversioned_RuntimeSpec_To_v1alpha3_RuntimeSpec(in *unversioned.Runtime, out *RuntimeSpec, s conversion.Scope) error {
-	return autoConvert_unversioned_RuntimeSpec_To_v1alpha3_RuntimeSpec(in, out, s)
+func Convert_unversioned_Runtime_To_v1alpha3_RuntimeSpec(in *unversioned.Runtime, out *RuntimeSpec, s conversion.Scope) error {
+	return autoConvert_unversioned_Runtime_To_v1alpha3_RuntimeSpec(in, out, s)
 }
