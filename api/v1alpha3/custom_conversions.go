4430bbbfcedadd8708529a56028034a59fc1b63c
ashnamehrotra <ashnamehrotra@gmail.com>
2023-12-01 14:09:34 -0800

    lint change unused scope variable to _
    Signed-off-by: ashnamehrotra <ashnamehrotra@gmail.com>

--- api/v1alpha3/custom_conversions.go
+++ api/v1alpha3/custom_conversions.go
@@ -13,7 +13,7 @@ func Convert_v1alpha3_ManagerConfig_To_unversioned_ManagerConfig(in *ManagerConf
 // TODO: change this to use unversioned.RuntimeSpec when unversioned is updated
 //
 //nolint:unused
-func autoConvert_v1alpha3_RuntimeSpec_To_unversioned_Runtime(in *RuntimeSpec, out *unversioned.Runtime, s conversion.Scope) error {
+func autoConvert_v1alpha3_RuntimeSpec_To_unversioned_Runtime(in *RuntimeSpec, out *unversioned.Runtime, _ conversion.Scope) error {
 	*out = unversioned.Runtime(in.Name)
 	return nil
 }
@@ -31,7 +31,7 @@ func Convert_unversioned_ManagerConfig_To_v1alpha3_ManagerConfig(in *unversioned
 // TODO: change this to use unversioned.RuntimeSpec when unversioned is updated
 //
 //nolint:unused
-func autoConvert_unversioned_Runtime_To_v1alpha3_RuntimeSpec(in *unversioned.Runtime, out *RuntimeSpec, s conversion.Scope) error {
+func autoConvert_unversioned_Runtime_To_v1alpha3_RuntimeSpec(in *unversioned.Runtime, out *RuntimeSpec, _ conversion.Scope) error {
 	out.Name = Runtime(string(*in))
 	out.Address = RuntimeAddress("")
 	return nil
