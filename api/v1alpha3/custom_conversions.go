04712383c5277c6561e188cea0576c915d2406b4
ashnamehrotra <ashnamehrotra@gmail.com>
2023-12-01 14:03:58 -0800

    try unsused
    Signed-off-by: ashnamehrotra <ashnamehrotra@gmail.com>

--- api/v1alpha3/custom_conversions.go
+++ api/v1alpha3/custom_conversions.go
@@ -5,39 +5,39 @@ import (
 	conversion "k8s.io/apimachinery/pkg/conversion"
 )
 
-//nolint:unparam,varcheck
+//nolint:unused
 func Convert_v1alpha3_ManagerConfig_To_unversioned_ManagerConfig(in *ManagerConfig, out *unversioned.ManagerConfig, s conversion.Scope) error {
 	return autoConvert_v1alpha3_ManagerConfig_To_unversioned_ManagerConfig(in, out, s)
 }
 
 // TODO: change this to use unversioned.RuntimeSpec when unversioned is updated
 //
-//nolint:unparam,varcheck
+//nolint:unused
 func autoConvert_v1alpha3_RuntimeSpec_To_unversioned_Runtime(in *RuntimeSpec, out *unversioned.Runtime, s conversion.Scope) error {
 	*out = unversioned.Runtime(in.Name)
 	return nil
 }
 
-//nolint:unparam,varcheck
+//nolint:unused
 func Convert_v1alpha3_RuntimeSpec_To_unversioned_Runtime(in *RuntimeSpec, out *unversioned.Runtime, s conversion.Scope) error {
 	return autoConvert_v1alpha3_RuntimeSpec_To_unversioned_Runtime(in, out, s)
 }
 
-//nolint:unparam,varcheck
+//nolint:unused
 func Convert_unversioned_ManagerConfig_To_v1alpha3_ManagerConfig(in *unversioned.ManagerConfig, out *ManagerConfig, s conversion.Scope) error {
 	return autoConvert_unversioned_ManagerConfig_To_v1alpha3_ManagerConfig(in, out, s)
 }
 
 // TODO: change this to use unversioned.RuntimeSpec when unversioned is updated
 //
-//nolint:unparam,varcheck
+//nolint:unused
 func autoConvert_unversioned_Runtime_To_v1alpha3_RuntimeSpec(in *unversioned.Runtime, out *RuntimeSpec, s conversion.Scope) error {
 	out.Name = Runtime(string(*in))
 	out.Address = RuntimeAddress("")
 	return nil
 }
 
-//nolint:unparam,varcheck
+//nolint:unused
 func Convert_unversioned_Runtime_To_v1alpha3_RuntimeSpec(in *unversioned.Runtime, out *RuntimeSpec, s conversion.Scope) error {
 	return autoConvert_unversioned_Runtime_To_v1alpha3_RuntimeSpec(in, out, s)
 }
