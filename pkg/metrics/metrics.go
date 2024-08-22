c1996fec365741f70766933b7fb26aebc8f6031a
ashnamehrotra <ashnamehrotra@gmail.com>
2024-08-22 16:02:44 -0700

    upgrade otel changes
    Signed-off-by: ashnamehrotra <ashnamehrotra@gmail.com>

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -8,11 +8,9 @@ import (
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
 	metric "go.opentelemetry.io/otel/metric"
-	"go.opentelemetry.io/otel/metric/instrument"
-	"go.opentelemetry.io/otel/metric/unit"
 	"go.opentelemetry.io/otel/sdk/instrumentation"
 	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
-	"go.opentelemetry.io/otel/sdk/metric/aggregation"
+	"go.opentelemetry.io/otel/sdk/metric/metricdata"
 )
 
 const (
@@ -36,8 +34,8 @@ func ConfigureMetrics(ctx context.Context, log logr.Logger, endpoint string) (sd
 
 	durationStream := sdkmetric.Stream{
 		Name: "imagejob_duration_run_seconds",
-		Unit: unit.Unit("s"),
-		Aggregation: aggregation.ExplicitBucketHistogram{
+		Unit: "s",
+		Aggregation: sdkmetric.AggregationExplicitBucketHistogram{
 			Boundaries: []float64{0, 10, 20, 30, 40, 50, 60},
 		},
 	}
@@ -52,56 +50,57 @@ func ConfigureMetrics(ctx context.Context, log logr.Logger, endpoint string) (sd
 func ExportMetrics(log logr.Logger, exporter sdkmetric.Exporter, reader sdkmetric.Reader) {
 	ctxB := context.Background()
 
-	m, err := reader.Collect(ctxB)
+	var rm metricdata.ResourceMetrics
+	err := reader.Collect(ctxB, &rm)
 	if err != nil {
 		log.Error(err, "failed to collect metrics")
 		return
 	}
-	if err := exporter.Export(ctxB, m); err != nil {
+	if err := exporter.Export(ctxB, &rm); err != nil {
 		log.Error(err, "failed to export metrics")
 	}
 }
 
 func RecordMetricsRemover(ctx context.Context, p metric.MeterProvider, totalRemoved int64) error {
-	counter, err := p.Meter("eraser").SyncInt64().Counter(ImagesRemovedCounter, instrument.WithDescription(ImagesRemovedDescription), instrument.WithUnit("1"))
+	counter, err := p.Meter("eraser").Int64Counter(ImagesRemovedCounter, metric.WithDescription(ImagesRemovedDescription), metric.WithUnit("1"))
 	if err != nil {
 		return err
 	}
 
-	counter.Add(ctx, totalRemoved, attribute.String("node name", os.Getenv("NODE_NAME")))
+	counter.Add(ctx, totalRemoved, metric.WithAttributes(attribute.String("node name", os.Getenv("NODE_NAME"))))
 	return nil
 }
 
 func RecordMetricsScanner(ctx context.Context, p metric.MeterProvider, totalVulnerable int) error {
-	counter, err := p.Meter("eraser").SyncInt64().Counter("vulnerable_images_run_total", instrument.WithDescription("total vulnerable images"), instrument.WithUnit("1"))
+	counter, err := p.Meter("eraser").Int64Counter("vulnerable_images_run_total", metric.WithDescription("total vulnerable images"), metric.WithUnit("1"))
 	if err != nil {
 		return err
 	}
 
-	counter.Add(ctx, int64(totalVulnerable), attribute.String("node name", os.Getenv("NODE_NAME")))
+	counter.Add(ctx, int64(totalVulnerable), metric.WithAttributes(attribute.String("node name", os.Getenv("NODE_NAME"))))
 	return nil
 }
 
 func RecordMetricsController(ctx context.Context, p metric.MeterProvider, jobDuration float64, podsCompleted int64, podsFailed int64) error {
-	duration, err := p.Meter("eraser").SyncFloat64().Histogram("imagejob_duration_run_seconds", instrument.WithDescription("duration of imagejob"), instrument.WithUnit(unit.Unit("s")))
+	duration, err := p.Meter("eraser").Float64Histogram("imagejob_duration_run_seconds", metric.WithDescription("duration of imagejob"), metric.WithUnit("s"))
 	if err != nil {
 		return err
 	}
 	duration.Record(ctx, jobDuration)
 
-	completed, err := p.Meter("eraser").SyncInt64().Counter("pods_completed_run_total", instrument.WithDescription("total pods completed"), instrument.WithUnit("1"))
+	completed, err := p.Meter("eraser").Int64Counter("pods_completed_run_total", metric.WithDescription("total pods completed"), metric.WithUnit("1"))
 	if err != nil {
 		return err
 	}
 	completed.Add(ctx, podsCompleted)
 
-	failed, err := p.Meter("eraser").SyncInt64().Counter("pods_failed_run_total", instrument.WithDescription("total pods failed"), instrument.WithUnit("1"))
+	failed, err := p.Meter("eraser").Int64Counter("pods_failed_run_total", metric.WithDescription("total pods failed"), metric.WithUnit("1"))
 	if err != nil {
 		return err
 	}
 	failed.Add(ctx, podsFailed)
 
-	jobTotal, err := p.Meter("eraser").SyncInt64().Counter("imagejob_run_total", instrument.WithDescription("total number of imagejobs completed"), instrument.WithUnit("1"))
+	jobTotal, err := p.Meter("eraser").Int64Counter("imagejob_run_total", metric.WithDescription("total number of imagejobs completed"), metric.WithUnit("1"))
 	if err != nil {
 		return err
 	}
